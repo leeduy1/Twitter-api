8c34ac44a020ac06c4060c70085ccd695fd3bd69
leeduy1 <levanduy.gv2003@gmail.com>
2023-08-15 00:38:30 +0700

    feat: verify email

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -3,7 +3,7 @@ import databaseService from './database.services'
 import { RegisterReqBody } from '~/models/requests/User.requests'
 import { hashPassword } from '~/utils/crypto'
 import { signToken } from '~/utils/jwt'
-import { TokenType } from '~/constants/enums'
+import { TokenType, UserVerifyStatus } from '~/constants/enums'
 import RefreshToken from '~/models/schemas/RefreshToken.schema'
 import { ObjectId } from 'mongodb'
 import { config } from 'dotenv'
@@ -18,6 +18,7 @@ class UserService {
         user_id,
         token_type: TokenType.AccessToken
       },
+      privateKey:  process.env.JWT_SECRET_ACCESS_TOKEN as string,
       options: {
         expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
       }
@@ -29,26 +30,43 @@ class UserService {
         user_id,
         token_type: TokenType.RefreshToken
       },
+      privateKey:  process.env.JWT_SECRET_REFRESH_TOKEN as string,
       options: {
         expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
       }
     })
   }
-  private signAcessAndRefreshToken(user_id: string) {
+  private signEmailVerifyToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.EmailVerifyToken
+      },
+      privateKey:  process.env.JWT_SECRET_EMAIL_VERIFY_TOKEN as string,
+      options: {
+        expiresIn: process.env.EMAIL_VERIFY_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+  private signAccessAndRefreshToken(user_id: string) {
     return Promise.all([this.signAccessToken(user_id), this.signRefreshToken(user_id)])
   }
   async register(payload: RegisterReqBody) {
-    const result = await databaseService.users.insertOne(
-      new User({ ...payload, date_of_birth: new Date(payload.date_of_birth), password: hashPassword(payload.password) })
+    const user_id = new ObjectId()
+    const email_verify_token = await this.signEmailVerifyToken(user_id.toString())
+
+    await databaseService.users.insertOne(
+      new User({ ...payload,_id:user_id ,email_verify_token ,date_of_birth: new Date(payload.date_of_birth), password: hashPassword(payload.password) })
     )
-    const user_id = result.insertedId.toString()
-    const [accessToken, refreshToken] = await this.signAcessAndRefreshToken(user_id)
+
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id.toString())
     await databaseService.refreshTokens.insertOne(
-      new RefreshToken({ user_id: new ObjectId(user_id), token: refreshToken })
+      new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token })
     )
+    console.log(email_verify_token);
     return {
-      accessToken,
-      refreshToken
+      access_token,
+      refresh_token
     }
   }
   async checkEmailExists(email: string) {
@@ -56,19 +74,39 @@ class UserService {
     return Boolean(user)
   }
   async login(user_id: string) {
-    const [accessToken, refreshToken] = await this.signAcessAndRefreshToken(user_id)
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
     await databaseService.refreshTokens.insertOne(
-      new RefreshToken({ user_id: new ObjectId(user_id), token: refreshToken })
+      new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token })
     )
     return {
-      accessToken,
-      refreshToken
+      access_token,
+      refresh_token
     }
   }
   async logout(refresh_token: string) {
     await databaseService.refreshTokens.deleteOne({ token: refresh_token })
     return {
       message: USERS_MESSAGES.LOGOUT_SUCCESS
+    }
+  }
+  async verifyEmail(user_id: string) {
+    const [token] = await Promise.all([this.signAccessAndRefreshToken(user_id), 
+      databaseService.users.updateOne(
+      {
+        _id: new ObjectId(user_id)
+      },
+      [{
+        $set: {
+          email_verify_token: '',
+          verify: UserVerifyStatus.Verified,
+          updated_at: "$$NOW"
+        }
+      }]
+    )])
+    const [access_token, refresh_token] = token
+    return {
+      access_token,
+      refresh_token
     }
   }
 }
